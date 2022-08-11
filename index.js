cbdb8fdaa78c67118f5ac0584f03e7f15e7127d4
Daniel <sicmylife@gmail.com>
2022-08-11 14:03:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('#open-btn');
+const closeBtn = document.querySelector('#close-btn');
+
+
+const navBar = document.querySelector('.navbar-nav');
+const navbarToggle = ()=> navBar.classList.toggle('active');
+
+hamburger.addEventListener('click',navbarToggle);
+closeBtn.addEventListener('click',navbarToggle);
